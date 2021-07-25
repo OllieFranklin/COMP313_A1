23171aaf8fcc89f1a8587304eba1ac59ea099451
Ollie Franklin <ollie.franklin3@gmail.com>
2021-07-25 15:08:30 +1200

    Add previous features back to project and clean up files.

--- PlatformerCPP/Intermediate/Build/Win64/PlatformerCPPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30040
-#include "C:/Users/Ollie/OneDrive/Documents/swen/comp313/assignments/assig1/COMP313_A1/PlatformerCPP/Intermediate/Build/Win64/PlatformerCPPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
