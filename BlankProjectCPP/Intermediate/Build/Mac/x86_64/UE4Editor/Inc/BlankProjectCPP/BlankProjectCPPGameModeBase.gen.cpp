be9f9597cdb10547cdbe4d6c7934e27ae3cb0fdd
Ollie Franklin <ollie.franklin3@gmail.com>
2021-07-26 12:46:17 +1200

    Clean up unused files.

---
+++ BlankProjectCPP/Intermediate/Build/Mac/x86_64/UE4Editor/Inc/BlankProjectCPP/BlankProjectCPPGameModeBase.gen.cpp
@@ -0,0 +1,87 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "BlankProjectCPP/BlankProjectCPPGameModeBase.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeBlankProjectCPPGameModeBase() {}
+// Cross Module References
+	BLANKPROJECTCPP_API UClass* Z_Construct_UClass_ABlankProjectCPPGameModeBase_NoRegister();
+	BLANKPROJECTCPP_API UClass* Z_Construct_UClass_ABlankProjectCPPGameModeBase();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_BlankProjectCPP();
+// End Cross Module References
+	void ABlankProjectCPPGameModeBase::StaticRegisterNativesABlankProjectCPPGameModeBase()
+	{
+	}
+	UClass* Z_Construct_UClass_ABlankProjectCPPGameModeBase_NoRegister()
+	{
+		return ABlankProjectCPPGameModeBase::StaticClass();
+	}
+	struct Z_Construct_UClass_ABlankProjectCPPGameModeBase_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_ABlankProjectCPPGameModeBase_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+		(UObject* (*)())Z_Construct_UPackage__Script_BlankProjectCPP,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_ABlankProjectCPPGameModeBase_Statics::Class_MetaDataParams[] = {
+		{ "Comment", "/**\n * \n */" },
+		{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+		{ "IncludePath", "BlankProjectCPPGameModeBase.h" },
+		{ "ModuleRelativePath", "BlankProjectCPPGameModeBase.h" },
+		{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_ABlankProjectCPPGameModeBase_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<ABlankProjectCPPGameModeBase>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_ABlankProjectCPPGameModeBase_Statics::ClassParams = {
+		&ABlankProjectCPPGameModeBase::StaticClass,
+		"Game",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x009002ACu,
+		METADATA_PARAMS(Z_Construct_UClass_ABlankProjectCPPGameModeBase_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_ABlankProjectCPPGameModeBase_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_ABlankProjectCPPGameModeBase()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_ABlankProjectCPPGameModeBase_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(ABlankProjectCPPGameModeBase, 4111315123);
+	template<> BLANKPROJECTCPP_API UClass* StaticClass<ABlankProjectCPPGameModeBase>()
+	{
+		return ABlankProjectCPPGameModeBase::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_ABlankProjectCPPGameModeBase(Z_Construct_UClass_ABlankProjectCPPGameModeBase, &ABlankProjectCPPGameModeBase::StaticClass, TEXT("/Script/BlankProjectCPP"), TEXT("ABlankProjectCPPGameModeBase"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(ABlankProjectCPPGameModeBase);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ BlankProjectCPP/Intermediate/Build/Mac/x86_64/UE4Editor/Inc/BlankProjectCPP/BlankProjectCPPGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BLANKPROJECTCPP_BlankProjectCPPGameModeBase_generated_h
+#error "BlankProjectCPPGameModeBase.generated.h already included, missing '#pragma once' in BlankProjectCPPGameModeBase.h"
+#endif
+#define BLANKPROJECTCPP_BlankProjectCPPGameModeBase_generated_h
+
+#define BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_SPARSE_DATA
+#define BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_RPC_WRAPPERS
+#define BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesABlankProjectCPPGameModeBase(); \
+	friend struct Z_Construct_UClass_ABlankProjectCPPGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABlankProjectCPPGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/BlankProjectCPP"), NO_API) \
+	DECLARE_SERIALIZER(ABlankProjectCPPGameModeBase)
+
+
+#define BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesABlankProjectCPPGameModeBase(); \
+	friend struct Z_Construct_UClass_ABlankProjectCPPGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABlankProjectCPPGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/BlankProjectCPP"), NO_API) \
+	DECLARE_SERIALIZER(ABlankProjectCPPGameModeBase)
+
+
+#define BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABlankProjectCPPGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABlankProjectCPPGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABlankProjectCPPGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABlankProjectCPPGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABlankProjectCPPGameModeBase(ABlankProjectCPPGameModeBase&&); \
+	NO_API ABlankProjectCPPGameModeBase(const ABlankProjectCPPGameModeBase&); \
+public:
+
+
+#define BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABlankProjectCPPGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABlankProjectCPPGameModeBase(ABlankProjectCPPGameModeBase&&); \
+	NO_API ABlankProjectCPPGameModeBase(const ABlankProjectCPPGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABlankProjectCPPGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABlankProjectCPPGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABlankProjectCPPGameModeBase)
+
+
+#define BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_12_PROLOG
+#define BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_SPARSE_DATA \
+	BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_RPC_WRAPPERS \
+	BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_INCLASS \
+	BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_SPARSE_DATA \
+	BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> BLANKPROJECTCPP_API UClass* StaticClass<class ABlankProjectCPPGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID BlankProjectCPP_Source_BlankProjectCPP_BlankProjectCPPGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
